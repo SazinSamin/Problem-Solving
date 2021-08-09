2479d1e6cf9eccd8b24461c0c2f1b73eb83c52ea
samin <sazinsamin@gmail.com>
2021-08-09 14:33:27 +0600

    Add several prgrame.

---
+++ C/5. Pointers in C/Pointers in C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void update(int *a, int *b){
+        int x = *a;
+        int y = *b;
+        *a = x + y;
+        *b = (x > y) ? (x - y) : (y - x);
+}
+
+int main(){
+        int a, b;
+        int *pa = &a, *pb = &b;
+
+        scanf("%d %d", &a, &b);
+        update(pa, pb);
+        printf("%d\n%d", a, b);
+
+        return 0;
+}
