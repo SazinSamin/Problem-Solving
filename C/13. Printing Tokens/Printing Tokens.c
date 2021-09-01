d6db9393ac285e18f8124502ea47b29a4a48bfce
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-01 22:42:36 +0600

    Delete C directory

--- C/13. Printing Tokens/Printing Tokens.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#define MAX 1000+1
-
-int main(){
-        char c;
-        while((c = getchar()) != '\n'){
-                (c == ' ') ? printf("\n") : printf("%c", c);
-        }
-}
