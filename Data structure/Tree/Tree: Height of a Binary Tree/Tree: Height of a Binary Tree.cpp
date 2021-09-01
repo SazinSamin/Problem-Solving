97036e30c93899a7b570607762a84aee2ac973b5
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-09-01 22:16:27 +0600

    Update Tree: Height of a Binary Tree.cpp

--- Data structure/Tree/Tree: Height of a Binary Tree/Tree: Height of a Binary Tree.cpp
+++ Data structure/Tree/Tree: Height of a Binary Tree/Tree: Height of a Binary Tree.cpp
@@ -38,19 +38,10 @@ class Node {
     Node* right;
 };
 
+//For understanding the height function.
+//https://github.com/SazinSamin/Samin_Reading_Room/blob/main/Data%20Structure/Tree/Binary%20search%20tree/Height/height.cpp
+
 */
-    // The height of a binary tree is the number of edges between the tree's root and its furthest leaf
-    // This function compute the height of the tree...
-    // We need two base case here...
-    // 1. If any node is NULL 
-    // 2. Another if a node right & left both subtree is NULL
-    // If we just use root == NULL base case we get height + 1
-    // bacause when we go deep, without middle base case we go to a position where node is NULL
-    // But when we return from there, as we recursively add 1 to compute height
-    // we add 1 here also, but there no edges between leaf and NULL pointer.
-    // So we add the middle base case to identify the leaf node and return from there
-    //But we still need 1st base case,because if a node have only one child, then another would be NULL, 
-    //So we have to return from there.
     
     int height(Node* root) {
        // 1st base case
