faad81362e3f0ce1c6b306350896cb9febdb31c3
Sazin Rishad Samin <sazinsamin50@gmail.com>
2021-12-28 19:17:06 +0600

    Create example_loopBased(TLE).cpp

---
+++ LeetCode/53. Maximum Subarray/example_loopBased(TLE).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int size = nums.size(), sum = 0, prev = INT_MIN;
+        for(int i=0; i<size; i++) {
+                for(int j=i; j<size; j++) {
+                        for(int k = i; k<=j; k++) {
+                                //  cout << nums[k] << " ";
+                                //  int v = sum;
+                                //  int x = nums[k];
+                                sum += nums[k];
+                        }
+                        //  cout << " : " << sum << endl;
+                        if(sum > prev) {
+                                prev = sum;
+                        }
+                        sum = 0;
+                }
+        }
+        return prev;
+    }
+};
+
+int main() {
+        vector<int> v = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Solution s = Solution();
+        cout << s.maxSubArray(v);
+}
