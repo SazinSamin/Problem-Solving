2836ef64d84f89ef526a9a38b5b1478428edd5fa
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-01-03 12:40:05 +0600

    Create 1114_Kadane's_Alog(123ms).cpp

---
+++ LeetCode/53. Maximum Subarray/1114_Kadane's_Alog(123ms).cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+        int maxSubArray(vector<int> &nums) {
+                int max = INT_MIN, sum = 0;
+                for(int i=0; i<nums.size(); i++) {
+                        sum += nums[i];
+                        if(sum > max) {
+                                max = sum;
+                        }
+                        if(sum < 0) {
+                                sum = 0;
+                        }
+                }
+                return max;
+        }
+};
