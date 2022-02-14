8dde387015e970e5e7f81d03207e59a03a10e7ed
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-02-14 08:46:05 +0600

    Update 0011_example3(faster_2).cpp

--- LeetCode/387. First Unique Character in a String/0011_example3(faster_2).cpp
+++ LeetCode/387. First Unique Character in a String/0011_example3(faster_2).cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    unsigned char array[27];
+        void initArray() {
+                for(int i=0; i<=26; i++) {
+                        array[i] = 0;
+                }
+        }
+    
+    int firstUniqChar(string s) {
+         initArray();
+                for(int i=0; i < s.length(); i++) {
+                        if (array[s[i] - 97] < 2){
+                                array[s[i] - 97]++;
+                        }
+                }
 
+
+        int idx = -1;
+        for(int i=0; i<s.length(); i++) {
+                if(array[s[i] - 97] == 1) {
+                        return i;
+                }
+        }
+
+        return idx;
+    }
+};
