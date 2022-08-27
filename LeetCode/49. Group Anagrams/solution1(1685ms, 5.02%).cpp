2bbaf2893c7ad3758421b7e925687fac6771c2ae
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-08-27 12:51:48 +0600

    Update solution1(1685ms, 5.02%).cpp

--- LeetCode/49. Group Anagrams/solution1(1685ms, 5.02%).cpp
+++ LeetCode/49. Group Anagrams/solution1(1685ms, 5.02%).cpp
@@ -3,6 +3,7 @@ public:
         vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<string> test = strs;
 
+        // sort all the words to get the a common between thems.
         for(int i = 0; i < test.size(); i++) {
                 sort(test[i].begin(), test[i].end());
         }
@@ -12,12 +13,18 @@ public:
         
         for(int i = 0; i < test.size(); i++) {
                 vector<int> n1;
+                // get the target
                 string first = test[i];
                 for(int j = 0; j < test.size(); j++) {
+                        // find the target every indexes & push the location to the vector
                         if(first == test[j]) {
                                 n1.push_back(j);
                         }
                 }
+                // as we some words repeat many time for sorting
+                // here we unordered_set to find out that in previouly we encouter this words or not
+                // if we are not encounter this words then we insert this on the unordered_set & also we push
+                // the indexes of that words.
                 if(idx.find(first) == idx.end()) {
                         idx.insert(first);
                         res.push_back(n1);
@@ -25,6 +32,7 @@ public:
         }
 
         vector<vector<string>> _final;
+        // get the words from the from the indexes & push them for final result.
         for(int i = 0; i < res.size(); i++) {
                 vector<string> temp;
                 for(int j = 0; j < res[i].size(); j++) {
