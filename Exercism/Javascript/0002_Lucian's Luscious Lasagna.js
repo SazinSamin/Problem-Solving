234dea1d5a48a745abd185a3f433f389b2ec0a09
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-06 20:37:19 +0600

    Update 0002_Lucian's Luscious Lasagna.js

--- Exercism/Javascript/0002_Lucian's Luscious Lasagna.js
+++ Exercism/Javascript/0002_Lucian's Luscious Lasagna.js
@@ -1,4 +1,4 @@
-// https://exercism.org/tracks/javascript/exercises/annalyns-infiltration
+// https://exercism.org/tracks/javascript/exercises/hello-world
 
 const PREPARATION_MINUTES_PER_LAYER = 2;
 export const EXPECTED_MINUTES_IN_OVEN = 40;
