7a10f05115f5beb76ba4c419d156b25a30607e5e
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-09-06 20:39:12 +0600

    Create 0001_Hello World.js

---
+++ Exercism/Javascript/0001_Hello World.js
@@ -0,0 +1,10 @@
+// https://exercism.org/tracks/javascript/exercises/hello-world
+
+
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export function hello() {
+  return 'Hello, World!';
+}
