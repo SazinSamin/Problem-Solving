b930ef5fe154af4651a8d3deebfc7f0aff4bd28d
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-24 09:32:07 +0600

    Create cpp1(19ms,77.00%).cpp

---
+++ LeetCode/9. Palindrome Number/cpp1(19ms,77.00%).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || (x != 0 && (x % 10 == 0))) return false;
+        int reverse = 0;
+        while(reverse < x) {
+                reverse = reverse * 10 + x % 10;
+                x /= 10;
+        }
+        return x == reverse || x == reverse / 10;
+    }
+};
