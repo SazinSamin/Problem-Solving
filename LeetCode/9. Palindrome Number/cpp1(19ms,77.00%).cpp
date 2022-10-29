f4d2feb8135fbc2484d8b1c1574c4608535c21ea
Sazin Rishad Samin <sazinsamin50@gmail.com>
2022-10-29 18:24:31 +0600

    Update cpp1(19ms,77.00%).cpp

--- LeetCode/9. Palindrome Number/cpp1(19ms,77.00%).cpp
+++ LeetCode/9. Palindrome Number/cpp1(19ms,77.00%).cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        // if the number is negative or 0 or divisible by 10 then return false immediatly
         if(x < 0 || (x != 0 && (x % 10 == 0))) return false;
         int reverse = 0;
+        
+        // reverse the of half the number
         while(reverse < x) {
                 reverse = reverse * 10 + x % 10;
                 x /= 10;
         }
+        // if number is even then x == reverse, if odd then cut the middle number and compare.
         return x == reverse || x == reverse / 10;
     }
 };
