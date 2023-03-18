7e5666503426d6926aca967595d3b3dabcf02a01
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-03-18 18:07:30 +0600

    Update Solution2(61ms,67%).cpp

--- LeetCode/448. Find All Numbers Disappeared in an Array/Solution2(61ms,67%).cpp
+++ LeetCode/448. Find All Numbers Disappeared in an Array/Solution2(61ms,67%).cpp
@@ -1,5 +1,8 @@
 // theory behind the algo -> https://medium.com/@saurav.agg19/find-all-numbers-disappeared-in-an-array-c6a01393909
 // The below code is my customization
+// here we mark a number as negative to determine the missing number
+// as we mark number as negative and we use the number for indexing so here every time access the number we have to make it positive
+// through abs, so that we avoid accessing negative index. (abs[nums[i] - 1) (this line)
 
 class Solution {
 public:
