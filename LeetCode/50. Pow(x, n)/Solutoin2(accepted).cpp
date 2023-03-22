35ebbd153a044d39c1d396232bfb3fad90b90ba7
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-03-22 10:55:52 +0600

    Update Solutoin2(accepted).cpp

--- LeetCode/50. Pow(x, n)/Solutoin2(accepted).cpp
+++ LeetCode/50. Pow(x, n)/Solutoin2(accepted).cpp
@@ -1,3 +1,10 @@
+// This solution is based on divide & conquer
+// if n is negative then make n positive and also the pow now will be inverse calculated (1/x)
+// Now if the power is even then we mutiply the number by itself or doubling the values like 2^4 and 2^4 = 2 ^ 8;
+// if odd then 2 * 2^n, extra odd number will multiply
+// here 2 example of x.
+
+
 class Solution {
 public:
     double myPow(double x, int n) {
