c078587a915a20dde1cd174c9b6cc1a48e3f8052
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-03-25 10:15:52 +0600

    Create Solution2(less Code).cpp

---
+++ LeetCode/118. Pascal's Triangle/Solution2(less Code).cpp
@@ -0,0 +1,17 @@
+// https://medium.com/nerd-for-tech/pascals-triangle-4acd190f6473
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+        for(int i = 0; i < numRows; i++) {
+            res[i].resize(i+1);
+            res[i][0] = res[i][i] = 1;
+
+            for(int j = 1; j < i; j++) {
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
+            }
+        }
+        return res;
+    }
+};
